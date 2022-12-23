fb2059aa9aac116a9631c91790adf37113e3d615
DebordCompany <debord.info@gmail.com>
2022-12-23 15:19:38 +0100

    Creada la seccion de tarjetas, hay algun punto en el que se sigue viendo la linea divisoria entre los dos div, practicamente no se aprecia pero ahi esta

---
+++ src/components/EnButton.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="container">
+        <div class="container_img">
+            <img src="../assets/img/membrete.svg" alt="" width="100%" height="auto">
+        </div>
+        
+        <div class="container_position">
+            <img src="../assets/img/pomo.png" alt="" class="container_position_img">
+            <div class="container_position_button"><p>ES/EN</p></div>
+        </div>
+    </div>
+</template>
+<style scoped>
+.container{
+    position: relative;
+    margin: 0;
+    
+}
+.container_position{
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: 0;
+    
+}
+.container_img img{
+    display: block;
+}
+.container_position_img{
+    width: 20%;
+    filter: drop-shadow(1px 1px 5px  rgba(0, 0, 0, 0.382));
+    margin: 0;
+}
+.container_position_button{
+    position: absolute;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 1em;
+}
+</style>
--- src/views/instalaciones.vue
+++ src/views/instalaciones.vue
@@ -1,3 +1,31 @@
+<script setup>
+import EnButton from "../components/EnButton.vue"
+</script>
 <template>
-    soy instalaciones
+    <div class="container max-width">
+         <EnButton/>
+         <div class="texto">
+            <h1>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugit mollitia ea, cum expedita harum laudantium molestiae quo aliquid, nihil, rem iste inventore ipsa nisi possimus? Itaque pariatur dignissimos mollitia rem.</h1>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corrupti aspernatur eos, rerum nisi porro veniam unde natus sapiente, ipsam repellat obcaecati hic magni eligendi adipisci odit ab ea! Tempora vitae dignissimos earum laboriosam non sint cupiditate et labore iusto unde. Dolorem, nam qui in impedit accusamus possimus. Blanditiis, velit perferendis.</p>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corrupti aspernatur eos, rerum nisi porro veniam unde natus sapiente, ipsam repellat obcaecati hic magni eligendi adipisci odit ab ea! Tempora vitae dignissimos earum laboriosam non sint cupiditate et labore iusto unde. Dolorem, nam qui in impedit accusamus possimus. Blanditiis, velit perferendis.</p>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corrupti aspernatur eos, rerum nisi porro veniam unde natus sapiente, ipsam repellat obcaecati hic magni eligendi adipisci odit ab ea! Tempora vitae dignissimos earum laboriosam non sint cupiditate et labore iusto unde. Dolorem, nam qui in impedit accusamus possimus. Blanditiis, velit perferendis.</p>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corrupti aspernatur eos, rerum nisi porro veniam unde natus sapiente, ipsam repellat obcaecati hic magni eligendi adipisci odit ab ea! Tempora vitae dignissimos earum laboriosam non sint cupiditate et labore iusto unde. Dolorem, nam qui in impedit accusamus possimus. Blanditiis, velit perferendis.</p>
+         </div>
+    </div>
+   
 </template>
+<style scoped>
+.container{
+    margin-top: 2em;
+}
+.texto{
+    background: var(--gris);
+    padding: 2em;
+    text-align: justify;
+    display: flex;
+    flex-direction: column;
+    gap: 1em;
+    color: white;
+    font-weight: 600;
+}
+</style>
